075146297a59377dfc48b5b24c5e038c2430191e
HiGal <f.galeev@innopolis.ru>
2018-11-10 16:41:43 +0300

    fixed bugs

--- static/micro.js
+++ static/micro.js
@@ -1,101 +1,67 @@
-//webkitURL is deprecated but nevertheless
-URL = window.URL || window.webkitURL;
+var record = document.querySelector("#mic");
+var state = 0;
 
-var gumStream; //stream from getUserMedia()
-var rec; //Recorder.js object
-var input; //MediaStreamAudioSourceNode we'll be recording
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+   console.log('getUserMedia supported.');
+   navigator.mediaDevices.getUserMedia (
+      // constraints - only audio needed for this app
+      {
+         audio: true
+      })
 
-// shim for AudioContext when it's not avb.
-var AudioContext = window.AudioContext || window.webkitAudioContext;
-var audioContext = new AudioContext; //new audio context to help us record
+      // Success callback
+      .then(function(stream) {
+          var mediaRecorder = new MediaRecorder(stream);
 
-var recordButton = document.getElementById("mic");
+          record.onclick = function() {
+              if(state === 0) {
+                  mediaRecorder.start();
+                  console.log(mediaRecorder.state);
+                  console.log("recorder started");
+                  record.style.color = "red";
+                  state++;
+              }else if(state === 1){
+                  mediaRecorder.stop();
+                  console.log(mediaRecorder.state);
+                  console.log("recorder stopped");
+                  record.style.background = "";
+                  record.style.color = "";
+                  state = 0;
+              }
+          };
 
-var status = 0;
 
-function mouseClick() {
-    if (status === 0){
-        alert(status);
-        startRecording();
-        status++;
-    }else if(status===1){
-        stopRecording();
-        status = 0;
+          mediaRecorder.addEventListener("dataavailable", onRecordingReady);
+
+      })
+
+      // Error callback
+      .catch(function(err) {
+         console.log('The following getUserMedia error occured: ' + err);
+      }
+   );
+
+   function onRecordingReady(e) {
+         var audio = document.getElementById('audio');
+         // e.data contains a blob representing the recording
+         audio.src = URL.createObjectURL(e.data);
+         audio.play();
+         var formData = new FormData();
+         var fileType = 'audio';
+         var fileName = 'output.wav';
+         formData.append(fileType, e.data, fileName);
+         $.ajax({
+            type: 'POST',
+            url: 'http://localhost/',
+            data: formData,
+            processData: false,  // prevent jQuery from converting the data
+            contentType: false,  // prevent jQuery from overriding content type
+            success: function(response) {
+            alert(response);
     }
+});
 }
 
-function startRecording() {
-
-    var constraints = {audio: true, video:false};
-    
-    navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-        console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
- 
-        /* assign to gumStream for later use */
-        gumStream = stream;
- 
-        /* use the stream */
-        input = audioContext.createMediaStreamSource(stream);
- 
-        /* 
-        Create the Recorder object and configure to record mono sound (1 channel)
-        Recording 2 channels  will double the file size
-        */
-        rec = new Recorder(input,{numChannels:1});
- 
-        //start the recording process
-        rec.record();
- 
-        console.log("Recording started");
- 
-    }).catch(function(err) {
-       
-    });
-    
-}
-
-function stopRecording() {
-    console.log("stopButton clicked");
-
-    //disable the stop button, enable the record too allow for new recordings
-    stopButton.disabled = true;
-    recordButton.disabled = false;
-    pauseButton.disabled = true;
-
-    //reset button just in case the recording is stopped while paused
-    pauseButton.innerHTML="Pause";
-
-    //tell the recorder to stop the recording
-    rec.stop();
-
-    //stop microphone access
-    gumStream.getAudioTracks()[0].stop();
-
-    //create the wav blob and pass it on to createDownloadLink
-    rec.exportWAV(createDownloadLink);
-}
-
-
-function createDownloadLink(blob) {
-
-    var url = URL.createObjectURL(blob);
-    var au = document.createElement('audio');
-    var li = document.createElement('li');
-    var link = document.createElement('a');
-
-    //add controls to the <audio> element
-    au.controls = true;
-    au.src = url;
-
-    //link the a element to the blob
-    link.href = url;
-    link.download = new Date().toISOString() + '.wav';
-    link.innerHTML = link.download;
-
-    //add the new audio and a elements to the li element
-    li.appendChild(au);
-    li.appendChild(link);
-
-    //add the li element to the ordered list
-    recordingsList.appendChild(li);
+} else {
+   console.log('getUserMedia not supported on your browser!');
 }
