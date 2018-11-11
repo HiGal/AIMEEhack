74a1283e80054c91317687f6e04eab67d2235a37
artyom-yurin <artem_yrin@mail.ru>
2018-11-11 11:10:23 +0300

    Added fucntion for handle voice message

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,9 @@ import requests
 import json
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-updater = Updater(token="570424499:AAGtsWf7uA_0BmbLyr5WDEE-7VT0mgh8biw")
+
+TELEGRAM_API_TOKEN = "570424499:AAGtsWf7uA_0BmbLyr5WDEE-7VT0mgh8biw"
+updater = Updater(token=TELEGRAM_API_TOKEN)
 dispatcher = updater.dispatcher
 
 print("Bot is alive")
@@ -14,7 +16,7 @@ def get_answer(text):
     print("LOG[JSON.QUERY]: " + str(json.dumps(response_text)))
     question = requests.post(url=url, data=response_text)
     response = question.json()
-    print("LOG[JSON.RESPONSE]: " + str(response) + " " + question + "\n")
+    print("LOG[JSON.RESPONSE]: " + str(response) + " " + str(question) + "\n")
     return response['message']
 
 
@@ -27,10 +29,21 @@ def text_message(bot, update):
     # {"Type": "film","Title" : " Титаник " ,"Released" : " 1997-11-18 ","Video":youtubelink,"Poster": urlpng,"Tagline":"Ничто на Земле не сможет разлучить их.","Score":"7.7","Status":"true"}
 
     if answer["Type"] == "film":
-        bot.send_message(chat_id=chat_id, text="Title: " + answer["Title"] + "\n" +
+        bot.send_message(chat_id    =chat_id, text="Title: " + answer["Title"] + "\n" +
                                                "Released:" + answer["Released"] + "\n")
     else:
         bot.send_message(chat_id=chat_id, text=answer)
+
+
+def voice_message(bot, update):
+    message = update.message
+    chat_id = message.chat_id
+    file_info = bot.get_file(message.voice.file_id)
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(TELEGRAM_API_TOKEN, file_info.file_path))
+    f = open('./voice.ogg', 'wb')
+    f.write(file.content)
+    f.close()
+    bot.send_message(chat_id=chat_id, text="Voice блять")
 
 
 def start_command(bot, update):
@@ -40,8 +53,10 @@ def start_command(bot, update):
 
 start_command_handler = CommandHandler('start', start_command)
 text_message_handler = MessageHandler(Filters.text, text_message)
+voice_message_handler = MessageHandler(Filters.voice, voice_message)
 
 dispatcher.add_handler(text_message_handler)
 dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(voice_message_handler)
 
 updater.start_polling(clean=True)
