e19f5c1db51703ced4ec6342040fa6be21a6e280
HiGal <f.galeev@innopolis.ru>
2018-11-11 14:17:50 +0300

    pochti final

--- aimee.py
+++ aimee.py
@@ -11,14 +11,15 @@ dialogue_id = 1
 
 
 def aimee_answer(data):
-    message_id= random.randint(1,100)
+    chat_id = random.randint(1,10)
+    message_id = random.randint(1,100)
     question = data['queryResult']['queryText']
     response = requests.get((instance + "/v2/ai/answer/?key={}&chat_id={}&message_id={}&dialogue_id={}") \
                           .format(question, chat_id, message_id, dialogue_id)).content
     response = json.loads(response)
-    print(response['hypothesis'][0]['answer'])
+    print(response['hypothesis'])
     reply = {
-        'fulfillmentText': response['hypothesis'][0]['answer']
+        'fulfillmentText': response['hypothesis'][1]['answer']
     }
     print(reply)
     return jsonify(reply)
